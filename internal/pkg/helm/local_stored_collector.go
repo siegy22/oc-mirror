d62ecc8136fbe16c8f6df7b34f66f0d60f8d8121
Yves Siegrist <Elektron1c97@gmail.com>
2025-05-26 23:30:38 +0200

    Allow custom values for helm charts to be provided
    Some helm charts don't render without providing values. Which forces one to use
    additionalImages a lot.
    Also a handy feature to reduce the additionalImages list, by setting the custom
    image (flavour) directly in the helm chart.
    
    One example would be OpenBao, that provides ubi based images, but doesn't use
    them by default.
    
    Before:
    
    ```yaml
    kind: ImageSetConfiguration
    apiVersion: mirror.openshift.io/v2alpha1
    mirror:
      # ...
      additionalImages:
        - quay.io/openbao/openbao-ubi:2.2.0
      helm:
        repositories:
          - name: bao
            url: https://openbao.github.io/openbao-helm
            charts:
              - name: openbao
                version: 0.12.0 # You'd have to manually check the matching image
                                # when bumping the chart's version
    ```
    
    After:
    
    ```
    kind: ImageSetConfiguration
    apiVersion: mirror.openshift.io/v2alpha1
    mirror:
      # ...
      helm:
        repositories:
          - name: bao
            url: https://openbao.github.io/openbao-helm
            charts:
              - name: openbao
                version: 0.12.0
                values:
                  server:
                    image:
                      repository: "openbao/openbao-ubi"
    ```

--- internal/pkg/helm/local_stored_collector.go
+++ internal/pkg/helm/local_stored_collector.go
@@ -126,8 +126,7 @@ func (o *LocalStorageCollector) HelmImageCollector(ctx context.Context) ([]v2alp
 					lsc.Log.Error("error pulling chart %s:%s", ref, err.Error())
 					continue
 				}
-
-				imgs, err := getImages(path, chart.ImagePaths...)
+				imgs, err := getImages(path, chart.Values, chart.ImagePaths...)
 				if err != nil {
 					errs = append(errs, err)
 				}
@@ -169,7 +168,7 @@ func (o *LocalStorageCollector) HelmImageCollector(ctx context.Context) ([]v2alp
 				src := filepath.Join(lsc.Opts.Global.WorkingDir, helmDir, helmChartDir)
 				path := filepath.Join(src, fmt.Sprintf("%s-%s.tgz", chart.Name, chart.Version))
 
-				imgs, err := getImages(path, chart.ImagePaths...)
+				imgs, err := getImages(path, chart.Values, chart.ImagePaths...)
 				if err != nil {
 					errs = append(errs, err)
 				}
@@ -224,7 +223,7 @@ func getHelmImagesFromLocalChart() ([]v2alpha1.RelatedImage, []error) {
 	var errs []error
 
 	for _, chart := range lsc.Config.Mirror.Helm.Local {
-		imgs, err := getImages(chart.Path, chart.ImagePaths...)
+		imgs, err := getImages(chart.Path, chart.Values, chart.ImagePaths...)
 		if err != nil {
 			errs = append(errs, err)
 		}
@@ -340,14 +339,14 @@ func getChartsFromIndex(indexURL string, indexFile helmrepo.IndexFile) ([]v2alph
 	for key, chartVersions := range indexFile.Entries {
 		for _, chartVersion := range chartVersions {
 			if chartVersion.Type != "library" {
-				charts = append(charts, v2alpha1.Chart{Name: key, Version: chartVersion.Version})
+				charts = append(charts, v2alpha1.Chart{Name: key, Version: chartVersion.Version, Values: make(map[string]interface{})})
 			}
 		}
 	}
 	return charts, nil
 }
 
-func getImages(path string, imagePaths ...string) (images []v2alpha1.RelatedImage, err error) {
+func getImages(path string, values map[string]interface{}, imagePaths ...string) (images []v2alpha1.RelatedImage, err error) {
 	lsc.Log.Debug("Reading from path %s", path)
 
 	p := getImagesPath(imagePaths...)
@@ -358,7 +357,7 @@ func getImages(path string, imagePaths ...string) (images []v2alpha1.RelatedImag
 	}
 
 	var templates string
-	if templates, err = getHelmTemplates(chart); err != nil {
+	if templates, err = getHelmTemplates(chart, values); err != nil {
 		return nil, fmt.Errorf("failed to get template %s: %w", chart.Name(), err)
 	}
 
@@ -388,20 +387,18 @@ func getImagesPath(paths ...string) []string {
 }
 
 // getHelmTemplates returns all chart templates
-func getHelmTemplates(ch *helmchart.Chart) (string, error) {
+func getHelmTemplates(ch *helmchart.Chart, values map[string]interface{}) (string, error) {
 	out := new(bytes.Buffer)
-	valueOpts := make(map[string]any)
 	caps := chartutil.DefaultCapabilities
 
-	if err := chartutil.ProcessDependencies(ch, valueOpts); err != nil {
+	if err := chartutil.ProcessDependencies(ch, values); err != nil {
 		return "", fmt.Errorf("error processing dependencies: %w", err)
 	}
+	valuesToRender, err := chartutil.ToRenderValues(ch, values, chartutil.ReleaseOptions{}, caps)
 
-	valuesToRender, err := chartutil.ToRenderValues(ch, valueOpts, chartutil.ReleaseOptions{}, caps)
 	if err != nil {
 		return "", fmt.Errorf("error rendering values: %w", err)
 	}
-
 	files, err := engine.Render(ch, valuesToRender)
 	if err != nil {
 		return "", fmt.Errorf("error rendering chart %s: %w", ch.Name(), err)
